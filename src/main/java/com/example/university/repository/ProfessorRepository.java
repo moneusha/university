e4c5d1ee86e3f1c4eeaa4b18394cf577912e6b4f
university <moneusha2016@gmail.com>
2024-08-07 04:43:55 +0000

    solution

---
+++ src/main/java/com/example/university/repository/ProfessorRepository.java
@@ -0,0 +1,32 @@
+package com.example.university.repository;
+
+
+import com.example.university.model.Course;
+import com.example.university.model.Professor;
+import org.springframework.stereotype.Repository;
+
+
+import java.util.List;
+
+
+@Repository
+public interface ProfessorRepository {
+
+
+    List<Professor> getProfessors();
+
+
+    Professor getProfessorById(Integer professorId);
+
+
+    Professor addProfessor(Professor professor);
+
+
+    Professor updateProfessor(int professorId, Professor professor);
+
+
+    void deleteProfessor(int professorId);
+
+
+    List<Course> getProfessorCourses(int professorId);
+}
